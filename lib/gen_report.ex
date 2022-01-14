c602bc1112e2ea36c25693f6f69bd4606b2c940d
André Araujo <andre.cristiano1975@gmail.com>
2022-01-14 08:53:51 +0000

    create a parse_file module

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,51 @@
 defmodule GenReport do
+  alias GenReport.Parser
 
-  def build do
-    # TO DO
+  @workers [
+    "cleiton",
+    "daniele",
+    "danilo",
+    "diego",
+    "giuliano",
+    "jakeliny",
+    "joseph",
+    "mayk",
+    "rafael",
+    "vinicius"
+  ]
+
+  @months [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
+
+  @years [
+    2016,
+    2017,
+    2018,
+    2019,
+    2020
+  ]
+
+  def build(_file_name), do: {:error, "Insira o nome de um arquivo"}
+
+  def build(file_name) do
+    file_name
+    |> Parser.parse_file()
+    #|> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
+
+  #defp sum_values([name, _value ], report), do: Map.put(report, name
+
+  #defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
--- lib/parser.ex
+++ lib/parser.ex
@@ -1,8 +1,34 @@
 defmodule GenReport.Parser do
+  @months %{
+    "1" => "janeiro",
+    "2" => "fevereiro",
+    "3" => "março",
+    "4" => "abril",
+    "5" => "maio",
+    "6" => "junho",
+    "7" => "julho",
+    "8" => "agosto",
+    "9" => "setembro",
+    "10" => "outubro",
+    "11" => "novembro",
+    "12" => "dezembro"
+  }
 
   def parse_file(filename) do
     filename
     |>File.stream!()
-    
+    |>Stream.map(fn line -> parse_line(line) end)
+    |>Enum.each(fn line -> IO.inspect(line) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |>String.trim()
+    |>String.split(",")
+    |>List.update_at(0, &String.downcase/1)
+    |>List.update_at(1, &String.to_integer/1)
+    |>List.update_at(2, &String.to_integer/1)
+    |>List.update_at(3, fn month -> @months[month] end)
+    |>List.update_at(4, &String.to_integer/1)
   end
 end
